56b9231328a2c8338ba9299b95ab1ccc9279dc76
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-02-09 23:22:03 +0100

    Update Person and classroom classes

--- book.rb
+++ book.rb
@@ -1,9 +1,9 @@
 class Book
+  attr_accessor :title, :author, :rentals
+
   def initialize(title, author)
     @title = title
     @author = author
     @rental = []
   end
-
-  attr_accessor :title, :author, :rentals
 end
